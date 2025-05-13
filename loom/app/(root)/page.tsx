912cf8a3c3684b5b061e654c4d0484a5f99ead86
V-M-B <vmbvarun@gmail.com>
2025-05-13 22:19:16 +0530

    feat: add VideoCard component and sample media assets

--- loom/app/(root)/page.tsx
+++ loom/app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/Header'
+import VideoCard from '@/components/VideoCard'
 import React from 'react'
 
 function page() {
@@ -8,6 +9,18 @@ function page() {
       <h1 className='text-2xl font-karla '>
         Welcome to SnapCast
       </h1>
+
+      <VideoCard
+      id="1"
+      title="SnapCast Message "
+      thumbnail="/assets/samples/thumbnail (1).png"
+      createdAt={new Date("2023-06-30:00:00")}
+      username="Alice"
+      views={10}
+      visibility="public"
+      duration={149}
+      userImg="/assets/images/jason.png"
+      />
 
     </main>
   )
---
+++ loom/components/VideoCard.tsx
@@ -0,0 +1,69 @@
+'use client'
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+
+const VideoCard = ({
+    id,
+    title,
+    thumbnail,
+    createdAt,
+    username,
+    views,
+    visibility,
+    duration,
+    userImg
+}:VideoCardProps) => {
+  return (
+    <Link href={`/video/${id}`} className='video-card'>
+        <Image src={thumbnail} alt="Thumbnail" width={290} height={160} className='thumbnail'/>
+        <article>
+            <div className="">
+                <figure>
+                    <Image
+                        src={userImg}
+                        alt="User"
+                        width={34}
+                        height={34}
+                        className="rounded-full aspect-square"
+                    />
+                    <figcaption>
+                        <h3>{username}</h3>
+                        <p>{visibility}</p>
+                    </figcaption>
+                </figure>
+                <aside>
+                    <Image
+                    src="/Assets/icons/eye.svg"
+                    alt="Views"
+                    width={16}
+                    height={16}
+                    />
+                    <span>
+                        {views}
+                    </span>
+                </aside>
+            </div>
+            <h2>
+                {title} -{" "}
+                {new Date(createdAt).toLocaleDateString('en-US', {
+                    year: 'numeric',
+                    month: 'short',
+                    day: 'numeric',
+                })}
+            </h2>
+        </article>
+        <button onClick={()=>{}} className='copy-btn'>
+            <Image
+                src="/assets/icons/link.svg"
+                alt="link"
+                width={18}
+                height={18}
+            />
+            <span>Copy</span>
+        </button>
+     </Link>   
+  )
+}
+
+export default VideoCard
