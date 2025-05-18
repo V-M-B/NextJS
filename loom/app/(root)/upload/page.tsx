5609aadd7ddea30e08d7917196d22c041953e725
V-M-B <vmbvarun@gmail.com>
2025-05-18 23:15:26 +0530

    feat: enhance upload page with FileInput and FormField components for better user input handling

--- loom/app/(root)/upload/page.tsx
+++ loom/app/(root)/upload/page.tsx
@@ -1,11 +1,27 @@
 'use client'
 import FileInput from '@/components/FileInput'
 import FormField from '@/components/FormField'
+import { visibilities } from '@/constants'
 import React from 'react'
 import { useState } from 'react'
 
 const page = () => {
+  const [formData, setFormData] = useState({
+    title: '',
+    description: '',
+    visibility:'public',
+  });
+
   const [error, setError] = useState<string | null>(null)
+
+
+const handleInputChnage = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const { name, value } = e.target;
+  setFormData((prev) => ({
+    ...prev,
+    [name]: value,
+  }));
+}
   return (
 <div className="wrapper page">
   <h1>Upload a video</h1>
@@ -14,8 +30,24 @@ const page = () => {
   <form action="" className='rounded-20 shadow-10 gap-6 w-full flex flex-col px-5 py-7.5 '>
 
       
-  <FileInput />
-  <FormField />
+  {/* <FileInput
+  
+  /> */}
+  <FormField 
+  id="title"
+  label="Title"
+  type="text"
+  value={formData.title}
+  onChange={handleInputChnage}
+  placeholder="Title of your video"
+  options={
+    [
+      { label: 'Public', value: 'public' },
+      { label: 'Unlisted', value: 'unlisted' },
+      { label: 'Private', value: 'private' },
+    ]
+  }
+  />
 
   
   </form>
