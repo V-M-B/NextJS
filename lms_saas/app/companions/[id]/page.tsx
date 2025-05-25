dda61953fe1d57caae149df016365771193dfdcc
V-M-B <vmbvarun@gmail.com>
2025-05-25 21:44:48 +0530

    Routes Setup

---
+++ lms_saas/app/companions/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>companinonsSession learning</div>
+  )
+}
+
+export default page
---
+++ lms_saas/app/companions/new/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>companinons form</div>
+  )
+}
+
+export default page
