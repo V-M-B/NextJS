b2c3cab3b80c294de194e7ed3d3df9d0e4c61015
V-M-B <vmbvarun@gmail.com>
2025-05-26 12:17:41 +0530

    Home Page Companion card with prop

--- lms_saas/app/page.tsx
+++ lms_saas/app/page.tsx
@@ -1,3 +1,6 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
@@ -5,11 +8,39 @@ const Page = () => {
   return (
     <div>
       <h1 className='text-2xl underline'>
-        Welcome to My SaaS App!
+       Popular Companions
       </h1>
-      <Button>
-        Lets get started
-      </Button>
+    <section className='home-section'>
+      <CompanionCard
+      id='1'
+      name='Companion One'
+      topic='Topic One'
+      subject='Mathematics'
+      duration={30}
+      color='#2196F3'
+      />
+      <CompanionCard
+      id='2'
+      name='Companion Two'
+      topic='Topic Two'
+      subject='Science'
+      duration={15}
+      color='#FF9800'
+      />
+      <CompanionCard
+      id='3'
+      name='Companion Three'
+      topic='Topic Three'
+      subject='History'
+      duration={36}
+      color='#4CAF50'
+      />
+
+    </section>
+    <section className='home-section'>
+      <CompanionList/>
+      <CTA/>
+    </section>
     </div>
   )
 }
