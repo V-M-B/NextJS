e497e8765936c2d969e871d783e7fbaaa56d961d
V-M-B <vmbvarun@gmail.com>
2025-05-30 19:58:02 +0530

    feat: implement CompanionLibrary with search and filter functionality

--- lms_saas/app/companions/page.tsx
+++ lms_saas/app/companions/page.tsx
@@ -1,18 +1,40 @@
-import CompanionForm from '@/components/CompanionForm'
+import CompanionCard from '@/components/CompanionCard';
+import SearchInputs from '@/components/SearchInputs';
+import SubjectFilter from '@/components/SubjectFilter';
+import { getAllCompanions } from '@/lib/actions/companinon.actions';
+import { getSubjectColor } from '@/lib/utils';
 import React from 'react'
 
-const page = () => {
-  return (
-    <main className='min-lg:w-1/3 min-md:w-2/3 items-center justify-center '>//
-      <article className='w-full gap-4 flex flex-col items-center justify-center p-4'>
-      <h1>
-        Companion Builder
-      </h1>
+const CompanionLibrary = async ({ searchParams }: SearchParams) => {
+ const filters = await searchParams;
+ const subject = filters.subject ? filters.subject : '';
+ const topic = filters.topic ? filters.topic : '';
 
-      <CompanionForm />
-      </article>
+const companions = await getAllCompanions({subject , topic}); 
+
+console.log('Companions:', companions);
+
+  return (
+    <main >
+      <section className='flex justify-between gap-4 max-sm:flex-col '>
+      <h1>Companion Library</h1>
+      <div className="flex gap-4">
+        <SearchInputs/>
+        <SubjectFilter/>
+      </div>
+      </section>
+
+      <section className='companions-grid'>
+      {companions.map((companion) => (
+        <CompanionCard key={companion.id}{...companion}
+        color={getSubjectColor(companion.subject) || 'bg-gray-200'}
+        />
+      ))}
+      </section>
     </main>
+
+  
   )
 }
 
-export default page 
+export default CompanionLibrary
