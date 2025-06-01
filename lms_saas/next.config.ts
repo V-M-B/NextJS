8b6d027daf1ce217eda97c572e567ed9bceffac7
V-M-B <vmbvarun@gmail.com>
2025-06-01 22:11:39 +0530

    feat: enhance CompanionSession with user authentication and companion details display; add CompanionComponent for interactive sessions vapi

--- lms_saas/next.config.ts
+++ lms_saas/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { hostname: "images.clerk.com" },
+      { hostname: "img.clerk.com" }, // Add this line
+    ],
+  },
 };
 
 export default nextConfig;
